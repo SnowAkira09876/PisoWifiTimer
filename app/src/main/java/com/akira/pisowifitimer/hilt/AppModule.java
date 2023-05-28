6db1d4157a4c77427853c1b94c52b10e95300524
Akira Snow <snowakira2814@gmail.com>
2023-05-29 05:43:51 +0800

    Updated hilt module

--- app/src/main/java/com/akira/pisowifitimer/hilt/AppModule.java
+++ app/src/main/java/com/akira/pisowifitimer/hilt/AppModule.java
@@ -10,16 +10,19 @@ import dagger.hilt.android.qualifiers.ApplicationContext;
 import dagger.hilt.components.SingletonComponent;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import javax.inject.Singleton;
 
 @Module
 @InstallIn(SingletonComponent.class)
 public class AppModule {
 
+  @Singleton
   @Provides
   public static AkiraRoom provideAkiraRoom(@ApplicationContext Context context) {
     return Room.databaseBuilder(context, AkiraRoom.class, "AkiraRoom").build();
   }
 
+  @Singleton
   @Provides
   public static ExecutorService provideExecutorService() {
     return Executors.newSingleThreadExecutor();
